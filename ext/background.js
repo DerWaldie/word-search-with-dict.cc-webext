a030345fcf99f2267dc47c34a1f3348efa10bf6d
Julius Rummel <32172513+DerWaldie@users.noreply.github.com>
2019-03-09 17:03:26 +0100

    first experimental version

---
+++ ext/background.js
@@ -0,0 +1,26 @@
+var URL_TEMPLATE = "http://www.dict.cc/?s=$WORD";
+
+function onError(error) {
+    console.log(error);
+}
+
+browser.contextMenus.create({
+    id: "word-selection",
+    title: browser.i18n.getMessage('contextMenuTitle'),
+    contexts: ['selection']
+});
+
+browser.contextMenus.onClicked.addListener(function(info, tab) {
+    if(info.menuItemId == "word-selection") {
+        var word = info.selectionText;
+        var newIndex = tab.index + 1;
+
+        browser.tabs.create({
+                active: true,
+                index: newIndex,
+                url: URL_TEMPLATE.replace("$WORD", encodeURIComponent(word))
+        }).catch(onError);
+    }
+});
+
+
--- extention/background.js
+++
@@ -1,5 +0,0 @@
-browser.contextMenus.create({
-    id: 'word-selection',
-    title: browser.i18n.getMessage('contextMenuTitle'),
-    contexts: ['selection']
-});
